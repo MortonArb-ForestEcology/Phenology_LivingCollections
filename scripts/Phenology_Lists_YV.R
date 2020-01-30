bd5fce4634aa72b3da9e8bcb92e201298b8c0ae2
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2020-01-30 16:01:50 -0600

    Fixed issue with %like% function
    Our method of checking the taxa in the list didn't play nice with multiple requirements so I changed it to split the Taxon name into Genus and Species so that we can just use the %in% function

--- scripts/Phenology_Lists_YV.R
+++ scripts/Phenology_Lists_YV.R
@@ -1,7 +1,6 @@
 #This is to pick out trees of interest for youth volunteering
 
 library (tidyr)
-library(data.table)
 library(readbulk)
 library(dplyr)
 path.out <- "G:/My Drive/LivingCollections_Phenology/Observing Lists/Youth_Volunteers/"
@@ -19,6 +18,8 @@ colnames(YV.sub) <- c("AcessionID", "Taxon", "CommonName", "GardenLocation", "Ga
                       "GardenSubgrid", "Latitude", "Longitude")
 
 
+YV.sub <- YV.sub %>% separate(Taxon, c("Genus", "Species"), " ", extra="merge")
+
 #Creating a list of garden locatins we want to pull out of the larger data frame
 gardenlist <- list("Childrens Garden", "Meadow Lake", "Firefly Pond", "Administration Building",
                    "Arbor Court", "Ground Cover Garden", "Hedge Garden", "Maze Garden", "Visitor Center")
@@ -26,9 +27,10 @@ gardenlist <- list("Childrens Garden", "Meadow Lake", "Firefly Pond", "Administr
 #Creating a list of taxa we want to pull out of the larger data frame
 taxalist <- list("Acer", "Ceris", "Fagus","Ginkgo", "Larix", "Liriodendron","Pinus","Quercus", "Ulmus")
 
+
 #Comparing the full list to our gardenlist to pull out what we want
 YV.org <- YV.sub[(YV.sub$GardenLocation %in% gardenlist),]
-YV.org <- YV.org[(YV.org$Taxon %like% taxalist),]
+YV.org <- YV.org[(YV.org$Genus %in% taxalist),]
 
 #Ordering my alphabetical order of column
 YV.org <- arrange(YV.org, GardenLocation)
