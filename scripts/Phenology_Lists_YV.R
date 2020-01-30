f63d5169d34d78cf3e1b08b9cadf93b3fa717679
BrendonReidy <breidy@mortonarb.org>
2020-01-30 12:53:00 -0600

    Update Phenology_Lists_YV.R
    Added the dplyr library (line 6)
    Added complete garden locations to the gadrden list (lines 23-24)
    Added all genera to the taxalist (line 27)

--- scripts/Phenology_Lists_YV.R
+++ scripts/Phenology_Lists_YV.R
@@ -3,7 +3,7 @@
 library (tidyr)
 library(data.table)
 library(readbulk)
-
+library(dplyr)
 path.out <- "G:/My Drive/LivingCollections_Phenology/Observing Lists/Youth_Volunteers/"
 
 setwd(path.out)
@@ -18,12 +18,13 @@ YV.sub <- subset(YV.dat, select = c(1:3, 10, 12:15))
 colnames(YV.sub) <- c("AcessionID", "Taxon", "CommonName", "GardenLocation", "GardenGrid",
                       "GardenSubgrid", "Latitude", "Longitude")
 
-View(YV.sub)
+
 #Creating a list of garden locatins we want to pull out of the larger data frame
-gardenlist <- list("Childrens Garden", "Meadow Lake", "Firefly Pond")
+gardenlist <- list("Childrens Garden", "Meadow Lake", "Firefly Pond", "Administration Building",
+                   "Arbor Court", "Ground Cover Garden", "Hedge Garden", "Maze Garden", "Visitor Center")
 
 #Creating a list of taxa we want to pull out of the larger data frame
-taxalist <- list("Acer")
+taxalist <- list("Acer", "Ceris", "Fagus","Ginkgo", "Larix", "Liriodendron","Pinus","Quercus", "Ulmus")
 
 #Comparing the full list to our gardenlist to pull out what we want
 YV.org <- YV.sub[(YV.sub$GardenLocation %in% gardenlist),]
