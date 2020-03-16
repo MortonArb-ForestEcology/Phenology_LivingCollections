c578bf39b22d5f083c1d7e57ad1ab41d41f94035
crollinson <crollinson@gmail.com>
2020-03-16 15:57:08 -0500

    clean.google updated to googlesheets4
    slight changes to what the output looks like; pay attention!

--- scripts/clean_google_form.R
+++ scripts/clean_google_form.R
@@ -1,10 +1,9 @@
-clean.google <- function(pheno.title = "Phenology_Observations_GoogleForm", collection="Quercus", dat.yr=lubridate::year(Sys.Date())){
-  pheno.lc <- gs_title(pheno.title)
-  pheno.lc
-  
+clean.google <- function(google.key = "1eEsiJ9FdDiNj_2QwjT5-Muv-t0e-b1UGu0AFBRyITSg", collection="Quercus", dat.yr=lubridate::year(Sys.Date())){
   # get the data from a particular sheet
-  dat.raw <- data.frame(gs_read(pheno.lc, ws=paste(collection, "Raw Observations", dat.yr, sep=" ")))
-
+  dat.raw <- data.frame(googlesheets4::read_sheet(ss=google.key, sheet=paste(collection, "Raw Observations", dat.yr, sep=" "), col_types="c"))
+  # names(dat.raw)
+  # summary(dat.raw[,1:10])
+  
   # Renaming some columns
   names(dat.raw)[grep("OPTIONAL", names(dat.raw))] <- "Notes"
   names(dat.raw)[grep("species", names(dat.raw))] <- "Species"
@@ -40,17 +39,35 @@ clean.google <- function(pheno.title = "Phenology_Observations_GoogleForm", coll
   pheno.fruit <- names(dat.raw)[grep("fruit", tolower(names(dat.raw)))]
   
   # Setting things to factors
-  for(i in 1:ncol(dat.raw)){
-    if(class(dat.raw[,i])=="character") dat.raw[,i] <- as.factor(dat.raw[,i])
-  }
-  summary(dat.raw)
+  # for(i in 1:ncol(dat.raw)){
+  #   if(class(dat.raw[,i])=="character") dat.raw[,i] <- as.factor(dat.raw[,i])
+  # }
+  # summary(dat.raw)
   cols.id <- grep("accession", names(dat.raw))
   
   dat.clean <- dat.raw[,c(cols.meta[!cols.meta=="PlantNumber"], pheno.leaf, pheno.flower, pheno.fruit)]
   dat.clean$PlantNumber <- as.factor(apply(dat.raw[,cols.id], 1, FUN=function(x) {x[which(!is.na(x))][1]})) # Get the PlantNumber
   dat.clean$Timestamp <- strptime(dat.clean$Timestamp, format="%m/%d/%Y %H:%M:%S")
   dat.clean$Date.Observed <- as.Date(dat.clean$Date.Observed, format="%m/%d/%Y")
+  dat.clean$Observer <- as.factor(dat.clean$Observer)
+  dat.clean$Species <- as.factor(dat.clean$Species)
 
+  # Converting everything that is not a date to a factor (except notes)
+  for(COL in c(pheno.leaf, pheno.flower, pheno.fruit)){
+    dat.clean[,COL] <- gsub(" ", "", dat.clean[,COL])
+    dat.clean[,COL] <- as.factor(dat.clean[,COL])
+    
+    # Now do the ordering based on the levels
+    if("Yes" %in% unique(dat.clean[,COL])){
+      dat.clean[,COL] <- car::recode(dat.clean[,COL], "'?'='Unsure'; 'Didnotlookfor'='No Observation'")
+      dat.clean[,COL] <- factor(dat.clean[,COL], levels=c("No", "Yes", "Unsure", "No Observation"))
+    } else if(any(grepl("%", dat.clean[,COL]))){
+      dat.clean[,COL] <- factor(dat.clean[,COL], levels=c("0%", "<5%", "5-24%", "25-49%", "50-74%", "75-94%", ">95%"))
+    } else {
+      dat.clean[,COL] <- factor(dat.clean[,COL], levels=c("0", "<3", "3-10", "11-100", "101-1,000", "1,001-10,000", ">10,000"))
+    }
+  }
+  
   dat.clean <- dat.clean[,c(cols.meta, pheno.leaf, pheno.flower, pheno.fruit)] # Just re-organizing to how I like to see things
   summary(dat.clean)
   
