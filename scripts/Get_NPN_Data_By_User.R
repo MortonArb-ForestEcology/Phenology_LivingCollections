67f2ac8a7ad00fdfa938abbe2db377682b034cb7
crollinson <crollinson@gmail.com>
2020-04-07 11:03:05 -0500

    Pull & analyze data by user
    Script to find sites belonging to users affilitated with The Morton Arboretum and pull their data!  Woot Woot!

--- scripts/Get_NPN_Data_By_User.R
+++ scripts/Get_NPN_Data_By_User.R
@@ -40,15 +40,37 @@ library(ggplot2)
 # -----------------------------------
 # 1. Get People in our Network 
 # -----------------------------------
-observers <- read.csv("")
+observers <- read.csv("../data/NPN/groupRoster_The_Morton_Arboretum.csv")
+summary(observers)
 # -----------------------------------
 
 # -----------------------------------
 # 2. Get sites for our people: 
 # -----------------------------------
+source("../../NPN_Data_Utils/R/npn_get_stations.R")
+
+dat.station <- data.frame()
+pb <- txtProgressBar(min=0, max=nrow(observers), style=3)
+for(i in 1:nrow(observers)){
+  setTxtProgressBar(pb, i)
+  dat.tmp <- npn.getStations(state_code="IL", person_id=observers$Person_ID[i], network_ids=NULL)
+  
+  dat.station <- rbind(dat.station, dat.tmp)
+}
+dat.station <- dat.station[!is.na(dat.station$station_id),]
+summary(dat.station)
 # -----------------------------------
 
 # -----------------------------------
 # 3. Get observations of our people's sites
 # -----------------------------------
+source("../../NPN_Data_Utils/R/npn_get_obs.R")
+
+# Note this step can be quite slow!
+# Note: Currently no observaitons beyond May 2018 :-(
+obs <- npn.getObs(station_id=dat.station$station_id, start_date = "2018-01-01")
+obs[obs==-9999] <- NA
+obs$phenophase_status <- car::recode(obs$phenophase_status, "'0'='no'; '1'='yes'; '-1'='uncertain'")
+obs$phenophase_status <- factor(obs$phenophase_status, levels=c("no", "yes", "uncertain"))
+summary(obs)
 # -----------------------------------
