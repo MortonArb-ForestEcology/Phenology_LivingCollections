6c036cb7e297339d7ecf3b5226f1d0ee3ba107ea
ShivenS04 <shivenrun@gmail.com>
2020-06-09 14:18:02 -0500

    Graph from Climate Data
    Created a scatter plot with maximum and minimum temps over first year but colors of points were inverted

--- scripts/SHIVEN_START_HERE.R
+++ scripts/SHIVEN_START_HERE.R
@@ -10,7 +10,18 @@
 # 0. Set up file paths, load libraries, etc.
 # 1. Download the latest weather data.  Can skip this if you already have data to work with
 # 2. Read in the data you have on hand and play around with it.
-# ---------------------------------
+# downloaded some random packages to see if they make it work
+library(rgdal)
+library(ggmap)
+library(raster)
+library(dplyr)
+library(leaflet)
+library(rgeos)
+library(tidyr)
+library(xts)
+library(dygraphs)
+library(sp)
+library(tigris)
 
 # ---------------------------------
 # 0. Set up some general file paths
@@ -24,8 +35,10 @@ dir.create(dir.met, recursive=T, showWarnings = F) # This will create the folder
 
 # ---------------------------------
 # 1. Download NOAA COOP Station data
+# Website I used to find information: https://cran.r-project.org/web/packages/rnoaa/rnoaa.pdf
+library(rnoaa)
 # Use FedData Package to download station data and put it here
-# ---------------------------------
+library("FedData")
 ID="USC00115097" # The Arb's weather station ID
 vars.want <- c("TMAX", "TMIN", "PRCP", "SNOW", "SNWD") # Variables we want to pull from the internet
 path.ghcn="../data_raw/meteorology/GHCN_extracted/"
@@ -39,9 +52,10 @@ dir.create(path.ghcn, recursive=T, showWarnings = F)
 # Repository URL: https://github.com/MortonArb-ForestEcology/Phenology_Forecasting
 # Note: this script might have some packages you need to install, so open it up and see what Rstudio tells you to install
 source("../../Phenology_Forecasting/scripts/met_download_GHCN.R")
+source("../../Phenology_Forecasting/scripts/met_gapfill.R")
+dir("../../")
 download.ghcn(ID=ID, vars.in=vars.want, path.save=path.ghcn, dir.raw=dir.raw, gapfill=T) # This downloads the data and puts it in the path.ghcn
 # -------------------------------------
-
 
 # -------------------------------------
 # 2. Read in the data you have on hand and play around with it.
@@ -51,4 +65,8 @@ dat.ghcn$DATE <- as.Date(dat.ghcn$DATE) # Converts this to a date format so it k
 summary(dat.ghcn) # 
 
 # CHALLENGE: Make 1 graph from the weather data.  Start with what you want to show.  Once you've done it, save it and post it to slack.  Also, save how you did it and commit it to Github!
-# -------------------------------------
+# I created a graph that displays the min and max temperatures for the first year since the data was too crowded for the entire dataset
+ggplot(dat.ghcn[1:365, ], aes(x= YDAY, y=TMAX, col="red")) + geom_point() + #maximum temp values: supposed to be red but are blue
+  geom_point(aes(x=YDAY, y=TMIN, col="blue")) + #minimum temp values: supposed to be blue but are red
+  labs(title="Temp Over the Year", x="Day of the Year", y="Temp Range (in Celsius)") #labels: struggled to change legend labels
+
