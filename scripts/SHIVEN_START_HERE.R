a6076f09cdbd2c4c61b165762d1efb5deea296bf
ShivenS04 <shivenrun@gmail.com>
2020-07-24 14:00:56 -0500

    Dropdown Testing
    further testing before meeting

--- scripts/SHIVEN_START_HERE.R
+++ scripts/SHIVEN_START_HERE.R
@@ -284,13 +284,7 @@ library(ggplot2)
 library(plotly)
 library(ggthemes)
 
-# # Sample workable data
-# 
-# sample <- data.frame("Country" = c("Australia", "Bangladesh",
-#                                    "Argentina", "Slovakia", "Greece", "Sweden"),
-#                      "Attribute1" = 57:62,
-#                      "Attribute2" = 13:18,
-#                      "Attribute3" = 5:10)
+
 # sample
 yearly_Textremes
 
@@ -327,7 +321,6 @@ ui <- fluidPage(
   )
 )
 
-
 # Define server logic required to draw a histogram: figured out how to do plotly with shiny
 server <- function(input, output) {
   
@@ -337,9 +330,120 @@ server <- function(input, output) {
       select(one_of(c("Year", input$selects2))) %>%
       gather(Attribute, value, -Year)
     
-    print(ggplotly(ggplot(data = data, aes(x = Year, y= value)) + 
-      geom_bar(aes(group = Attribute, fill = Year), stat = "identity", color = "blue")))
+    print(ggplotly(ggplot(data = data, aes(x = grp, y= value)) + 
+      geom_bar(aes(group = Attribute, fill = Year, 
+                   # text = sprintf("Group: %s", paste(grp)) #did not work when I tried to use this to add extra hover info
+                   ), stat = "identity", color = "blue")))
     })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+#copied from internet program which filters which data is being used: not working
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(DT)
+library(readxl)
+
+#this line is not working
+load("movies.Rdata")
+
+d_title_type <- sort(unique(movies$title_type))
+d_genre <- sort(unique(movies$genre))
+d_mpaa_rating <- sort(unique(movies$mpaa_rating))
+d_studio <- sort(unique(movies$studio))
+min_year <- min(movies$thtr_rel_year)
+max_year <- max(movies$thtr_rel_year)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(width = 2,
+                 selectizeInput(
+                   inputId = "i_title_type",
+                   label = "Title type:",
+                   choices = d_title_type,
+                   selected = NULL,
+                   multiple = TRUE,
+                   options = list(placeholder = "Begin typing title type...")
+                 ),
+                 
+                 selectizeInput(
+                   inputId = "i_genre",
+                   label = "Genre:",
+                   choices = d_genre,
+                   selected = NULL,
+                   multiple = TRUE,
+                   options = list(placeholder = "Begin typing genre...")
+                 ),
+                 
+                 selectizeInput(
+                   inputId = "i_studio",
+                   label = "Studio:",
+                   choices = d_studio,
+                   selected = NULL,
+                   multiple = TRUE,
+                   options = list(placeholder = "Begin typing studio...")
+                 ),
+                 
+                 selectizeInput(
+                   inputId = "i_mpaa_rating",
+                   label = "MPAA rating:",
+                   choices = d_mpaa_rating,
+                   selected = NULL,
+                   multiple = TRUE,
+                   options = list(placeholder = "Begin typing mpaa rating...")
+                 ),
+                 
+                 sliderInput(
+                   inputId = "i_year",
+                   label = "Year", min = min_year, max = max_year,
+                   value = c(1995, 2000)
+                   
+                 ),
+                 
+                 br(), br(),
+                 
+                 downloadButton("download", "Download results")
+                 
+    ),
+    
+    mainPanel(width = 10,
+              DT::dataTableOutput(outputId = "mtable")
+    )
+  )
+)
+
+
+server <- function(input, output) {
+  
+  mtable <- reactive({
+    filter(movies, (title_type %in% input$i_title_type) &
+             (genre %in% input$i_genre) &
+             (studio %in% input$i_studio) &
+             (mpaa_rating %in% input$i_mpaa_rating) &
+             (year >= input$year[1] & year <= input$year[2])) %>%
+      select(title:thtr_rel_year) 
+  })
+  
+  output$mtable <- DT::renderDataTable({
+    DT::datatable(data = mtable(), options = list(pageLength = 10),
+                  rownames = FALSE, class = 'display', escape = FALSE)
+    
+  })
+  
+  output$download <- downloadHandler(
+    filename = function() {
+      "movie-results.csv"
+    },
+    content = function(con) {
+      write.csv(mtable(), con)
+    }
+  )
 }
 
 # Run the application 
