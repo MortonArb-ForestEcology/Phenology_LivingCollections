1d530045d5aacecc634742620ef076ff9f399ff0
Christy Rollinson <crollinson@gmail.com>
2021-07-30 15:34:44 -0500

    add some stuff to pull current 2021 NPN obs for stats

--- scripts/2021-Mid_Year Report_ExtraStats.R
+++ scripts/2021-Mid_Year Report_ExtraStats.R
@@ -1,1 +1,16 @@
 # Additional stats for the mid-year report
+
+source("../../NPN_Data_Utils/R/npn_get_obs.R")
+source("../../NPN_Data_Utils/R/npn_enter_observation.R")
+source("clean_google_form.R")
+npn_creds <- readLines("../data/NPN/.NPN_creds")
+user_id <- strsplit(npn_creds[1], ":")[[1]][2]
+user_pw <- strsplit(npn_creds[2], ":")[[1]][2]
+# Loop through collections
+sites.push <- c("Oak", "Maple", "Elm")
+yrs.push <- c(lubridate::year(Sys.Date()))
+
+stat.arb <- read.csv("../data/NPN/NPN_TheMortonArboretum_Stations_All.csv")
+
+dat.npn <- rnpn::npn_download_status_data(station_ids=unique(stat.arb$station_id), years=2021, request_source="C. Rollinson, Morton Arb")
+dat.npn$observation_date <- as.Date(dat.npn$observation_date)
