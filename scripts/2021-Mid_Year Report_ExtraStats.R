218ebbd3b09006eae9143eb82288d4fec749f614
Christy Rollinson <crollinson@gmail.com>
2021-08-04 13:57:32 -0500

    add challenge graphs for @BrendonReidy

--- scripts/2021-Mid_Year Report_ExtraStats.R
+++ scripts/2021-Mid_Year Report_ExtraStats.R
@@ -3,7 +3,7 @@
 source("../../NPN_Data_Utils/R/npn_get_obs.R")
 source("../../NPN_Data_Utils/R/npn_enter_observation.R")
 source("clean_google_form.R")
-npn_creds <- readLines("../data/NPN/.NPN_creds")
+# npn_creds <- readLines("../data/NPN/.NPN_creds")
 user_id <- strsplit(npn_creds[1], ":")[[1]][2]
 user_pw <- strsplit(npn_creds[2], ":")[[1]][2]
 # Loop through collections
@@ -14,3 +14,16 @@ stat.arb <- read.csv("../data/NPN/NPN_TheMortonArboretum_Stations_All.csv")
 
 dat.npn <- rnpn::npn_download_status_data(station_ids=unique(stat.arb$station_id), years=2021, request_source="C. Rollinson, Morton Arb")
 dat.npn$observation_date <- as.Date(dat.npn$observation_date)
+
+summary(dat.npn)
+
+# Children's Garden Data
+dat.cg <- dat.npn[dat.npn$site_id == 37197,]
+
+# Meadow Lake Data
+dat.ml <- dat.npn[dat.npn$site_id == 37196,]
+
+
+# Weekly Challenge Graphs:
+# Week 1. # obs pers list per week (or through time)
+# Week 2. # phenophases observed in each list; bonus points: break down through time
