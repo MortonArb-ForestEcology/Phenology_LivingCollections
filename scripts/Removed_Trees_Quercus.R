d0800b7c298fa45c8b9b6b21b2f3ce5348ccb036
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2022-02-04 14:12:51 -0600

    Update Removed_Trees_Quercus.R
    Updated comments

--- scripts/Removed_Trees_Quercus.R
+++ scripts/Removed_Trees_Quercus.R
@@ -4,26 +4,28 @@ library (tidyr); library(dplyr); library(readbulk); library(googlesheets4)
 setwd("~/Google Drive/My Drive/LivingCollections_Phenology")
 dir.base <- "Google Drive/My Drive/LivingCollections_Phenology"
 
-path.dat <- file.path(dir.base, "Observing Lists")
-path.gone <- file.path(dir.base, "Data_Observations")
+
 path.out <- "Google Drive/My Drive/LivingCollections_Phenology/Observing Lists"
 
 
-#Loading tree lists and place in data frame dat.all
+#Batch loading trees from the Quercus tree observation lists and placing them in data frame dat.all
 dat.all <- read_bulk(directory = "../LivingCollections_Phenology/Observing Lists/Quercus", extension = ".csv", header=FALSE,)
 head(dat.all)
 
+#Up dating col name from V1, V2, etc to reflect the information in those columns 
 colnames(dat.all)<- c("Obs.List", "PlantNumber", "Taxon", "Vernacular","BgLatitude", "BgLongitude","GardenGrid", "GardenSubGrid", "File")
 
 summary(dat.all)
 head(dat.all)
 
+#Loading the Removed trees google sheet and placing it in a data frame
 dat.gone <- read_sheet("https://docs.google.com/spreadsheets/d/16xMa6MyJlh3zKkELrDToyoPk_GfoN1NSCVji_ttOCoQ/edit#gid=0")
 df.gone <-data.frame(dat.gone, header=FALSE)
 
 head(df.gone)
 
-#Joinging two data frames with anti_join, to remove all the values for PlantNumber that match between dat.all and df.gone
+#Joinging two data frames with anti_join, to remove all the values for PlantNumber that match 
+#between dat.all and df.gone and placing them in the new data frame new.dat
 new.dat <- anti_join(dat.all, df.gone, by=("PlantNumber"))
 summary(new.dat)
 
@@ -31,12 +33,14 @@ summary(new.dat)
 colnames(new.dat)<- c("Obs.List", "PlantNumber", "Taxon", "Vernacular","BgLatitude", "BgLongitude","GardenGrid", "GardenSubGrid", "Spacer")
 head(new.dat)
 
-# Split dataframe by the "Spacer" column
+# Splitting the new.dat dataframe by names in the the "Spacer" column, splits the data into
+# the updated observations lists, and the list containing all oaks. 
 split_new.dat <- split(new.dat, list(new.dat$Spacer))
 
-# Loop to write out new .csv files  based upon the splits created in the split_new.dat (based around the "Spacer" col. 
-# Will create the updated observation lists 
-######Change file path out#######
+# Loop to write out new .csv filesbased upon the splits created in the split_new.dat 
+
+######Change file path to path.out if everything works correctly#######
+
 for (Spacer in names(split_new.dat)) {
   write.csv(split_new.dat[[Spacer]], paste0("~/Desktop/R junk/", Spacer, ".csv"),)
 }
