b2fc8073fcd768bde180c96f6c1e9092183a1826
Christy Rollinson <crollinson@gmail.com>
2022-02-09 13:02:43 -0600

    basic script for getting our observations off of npn

---
+++ scripts/get_NPN_obs_basic.R
@@ -0,0 +1,9 @@
+# Read in Arb station codes
+# Data frame if we want to pull individual sites
+stat.arb <- read.csv("../data/NPN/NPN_TheMortonArboretum_Stations_All.csv")
+
+id.arb <- 720 # Our USA-NPN network ID
+
+dat.npn <- rnpn::npn_download_status_data(network_ids=id.arb, years=2021, request_source="C. Rollinson, Morton Arb")
+summary(dat.npn)
+nrow(dat.npn) #Number of observations (counts each phenophase separately)
