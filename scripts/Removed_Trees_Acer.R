f1eca5100530df01adf03dff1b7ea51b662c3280
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2022-02-18 13:19:12 -0600

    Create Removed_Trees_Acer.R
    Creating observing lists of trees in the acer collection without removed trees present.

---
+++ scripts/Removed_Trees_Acer.R
@@ -0,0 +1,59 @@
+library (tidyr); library(dplyr); library(readbulk); library(googlesheets4)
+
+#Making sure the working directory in is google drive. 
+setwd("~/Google Drive/My Drive/LivingCollections_Phenology")
+dir.base <- "Google Drive/My Drive/LivingCollections_Phenology/Observing Lists"
+
+path.google <- "~/GoogleDrive/My Drive" # Mac
+path.out <- "~/Google Drive/My Drive/LivingCollections_Phenology/Observing Lists/Acer/"
+
+#Batch loading trees from the Acer tree observation lists and placing them in data frame dat.all
+dat.all <- read_bulk(directory = "../LivingCollections_Phenology/Observing Lists/Acer", extension = ".csv", header= TRUE,)
+head(dat.all)
+
+dat.all <- subset(dat.all, select = c(1:8))
+
+#head(dat.all)
+#Up dating col name from V1, V2, etc to reflect the information in those columns 
+#colnames(dat.all)<- c("Obs.List", "PlantNumber", "Taxon", "Vernacular","BgLatitude", "BgLongitude","GardenGrid", "GardenSubGrid", "Spacer")
+
+
+summary(dat.all)
+head(dat.all)
+
+#Loading the Removed trees google sheet and placing it in a data frame
+dat.gone <- read_sheet("https://docs.google.com/spreadsheets/d/16xMa6MyJlh3zKkELrDToyoPk_GfoN1NSCVji_ttOCoQ/edit#gid=0")
+df.gone <-data.frame(dat.gone, header=TRUE)
+
+head(df.gone)
+
+#Joinging two data frames with anti_join, to remove all the values for PlantNumber that match 
+#between dat.all and df.gone and placing them in the new data frame new.dat
+new.dat <- anti_join(dat.all, df.gone, by=("PlantNumber"), row.names=F)
+
+#deleting duplicates
+new.dat <- arrange(new.dat, Obs.List)
+new.dat <- new.dat[!duplicated(new.dat$PlantNumber), ]
+summary(new.dat)
+
+head(new.dat)
+
+#Writing a csv of new.dat to be our Acer all list
+
+write.csv(new.dat, paste0(path.out, "ObservingList_Acer.csv"), row.names = F)
+
+write.csv(new.dat, paste0("~/Desktop/R junk/", "ObservingList_Acer.csv"), row.names = F)
+
+# Splitting the new.dat dataframe by names in the the "Obs.List" column, splits the data into
+# the updated observations lists, and the list containing all oaks. 
+split_new.dat <- split(new.dat, list(new.dat$Obs.List))
+
+# Loop to write out new .csv files based upon the splits created in the split_new.dat 
+
+for (Obs.List in names(split_new.dat)) {
+  write.csv(split_new.dat[[Obs.List]], paste0("~/Desktop/R junk/","ObservingList_Acer_",Obs.List,".csv"), row.names = F,)
+}
+
+
+
+
