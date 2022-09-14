b13e6f2ca036658a939d8082428fb8b6f703022d
Christy Rollinson <crollinson@gmail.com>
2022-09-14 12:48:00 -0500

    Fine name updates
    give annual/mid-year report updates a consistent naming structure

--- scripts/MortonArb_Phenology_AnnualReport_2022-06_Spring.R
+++ scripts/MortonArb_Phenology_AnnualReport_2022-06_Spring.R
@@ -388,3 +388,17 @@ ggplot(data=meanileaf.present) +
   labs(title="Mean Period of Leaves Present", x="Day of Year")
 dev.off()
 
+
+
+
+#### -----------------------------
+#### Doing a quick check on our contributions to USA-NPN
+#### -----------------------------
+stat.arb <- read.csv("../data/NPN/NPN_TheMortonArboretum_Stations_All.csv")
+
+id.arb <- 720 # Our USA-NPN network ID
+
+dat.npn <- rnpn::npn_download_status_data(network_ids=id.arb, years=2022, request_source="C. Rollinson, Morton Arb")
+summary(dat.npn)
+nrow(dat.npn) #Number of observations (counts each phenophase separately)
+#### -----------------------------
