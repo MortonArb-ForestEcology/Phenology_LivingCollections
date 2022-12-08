cce1ab7cd8e5b97ca88b19ddff42de3137556286
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2022-12-08 13:28:59 -0600

    Update 2022_tree_senescence.R
    Fixed data to show dates After August 1st.
    Tried to fix some ggplot weirdness, failed.

--- scripts/2022_tree_senescence.R
+++ scripts/2022_tree_senescence.R
@@ -1,5 +1,7 @@
 # A new script with to attempt to get 50% senescence metric for leaf color in 2022
 # -----------------------------------
+library(ggplot2)
+#------------------------------------
 source("clean_google_form.R")
 # Reading phenooogy data for 2022
 acer22 <- clean.google(collection="Acer", dat.yr=2022)
@@ -28,18 +30,33 @@ dat.22 <- rbind(quercus22,acer22, ulmus22, tilia22)
 dat.22$yday <- lubridate::yday(dat.22$Date.Observed)
 summary(dat.22)
 
+
+#only looking at trees that showed fall color from 8/1 on
+dat.22 <- dat.22 [dat.22$yday>=150,]
+
 #Seperating out the columns I want. I might be able to skip this step, however,I wanted to make sure I was only grabbing trees that recorded "Yes" for leaves.
-dat.se <- dat.22[dat.22$leaf.present.observed=="Yes", c("Date.Observed", "Species", "PlantNumber", "leaf.color.intensity", "leaf.present.intensity")]
+dat.se <- dat.22[dat.22$leaf.present.observed=="Yes", c("Date.Observed", "Species", "PlantNumber", "leaf.color.intensity", "Collection", "leaf.present.intensity")]
 summary(dat.se)
 
 ##Creating a df containing only leaves that expressed the critera of senescence 
 dat.22o <-dat.22[dat.22$leaf.present.intensity %in% c( "<5","5-24%", "25-49%")| dat.22$leaf.color.intensity %in% c(">95", "75-94%", "50-74%"),]
 head(dat.22o)
 
-#aggregating out NA'S and superfulous columns 
-leaf.sen <- aggregate(yday ~ PlantNumber + Species , data=dat.22o, FUN=min, na.rm=T)
+#aggregating out NA'S and superfluous columns 
+leaf.sen <- aggregate(yday ~ PlantNumber + Species + Collection, data=dat.22o, FUN=min, na.rm=T)
 summary(leaf.sen)
 head(leaf.sen)
 
-#writing .csv
+#writing .csv commented out for now
 write.csv(leaf.sen,"~/Google Drive/My Drive/LivingCollections_Phenology/Data_Observations//Tree_senescence__TMA_2022.CSV")
+
+#Graphing
+ggplot(data=leaf.sen) +
+  facet_grid(Collection~.,scales="free_y" ) + 
+  geom_point(alpha=0.5, aes(x=yday, y=PlantNumber, fill=as.factor(yday), color=as.factor(yday))) +
+ #theme(axis.title.y=element_blank(),
+        #axis.text.y=element_blank(),
+       # axis.ticks.y=element_blank())+
+  theme(legend.position = "none")+
+  labs(title="testing", x="Day of Year")
+dev.off()
