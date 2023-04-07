c72ec31f8f36de7dcfff3bbac01d91933d24d3c8
Christy Rollinson <crollinson@gmail.com>
2023-04-07 15:47:50 -0500

    QAQC script updates
    now including checking values in the observation form against the other tables in the database

--- scripts/Phenology_GetData_SQL.R
+++ scripts/Phenology_GetData_SQL.R
@@ -21,28 +21,79 @@ conn <- dbConnect(
   port    = 5432
 )
 
-TEST <- dbReadTable(conn, "FormSubmission")
+observers <- dbReadTable(conn, "Observers")
+treeLists <- dbReadTable(conn, "ObservingLists")
+datAll <- dbReadTable(conn, "FormSubmission")
 
 ### DISCONNECT FROM DATABASE
 dbDisconnect(conn)
 
-summary(TEST)
-for(COL in names(TEST)[!names(TEST) %in% c("DateEntered", "DateObserved")]){
-  TEST[,COL] <- as.factor(TEST[,COL])
+summary(datAll)
+for(COL in names(datAll)[!names(datAll) %in% c("DateEntered", "DateObserved")]){
+  datAll[,COL] <- as.factor(datAll[,COL])
 }
 
-TEST[TEST==""] <- NA
-TEST <- droplevels(TEST)
-summary(TEST)
+datAll[datAll==""] <- NA
+datAll <- droplevels(datAll)
 
-# Checking for immediate issues
-TEST[!TEST$Genus %in% c("Quercus", "Acer", "Ulmus"), ]
-# PlantIDAll <- unique(TEST$PlantID)
-TEST[grepl("[/]", TEST$PlantID),]
-TEST[!grepl("[-]", TEST$PlantID) | !grepl("[*]", TEST$PlantID),]
+# cleaning up some relatively minor issues with the data
+# # datAll$Genus <- gsub(" ", "", datAll$Genus)
+# datAll$Species <- gsub("  ", " ", datAll$Species)
+# datAll$Species <- tolower(datAll$Species)
+# summary(datAll)
 
-summary(TEST$ObserverID)
+# Checking for immediate issues all related to bad entry
+# Checking for observers not in our existing ID list
+datAll[!datAll$ObserverID %in% c(observers$ObserverID, "UNKNOWN"),]
+
+# Checking for trees whose PlantID isn't in our list
+datAll[!datAll$PlantID %in% c(treeLists$PlantID),] 
+
+# Genus not in the three we're focusing on
+datAll[!datAll$Genus %in% c("Quercus", "Acer", "Ulmus"), ]
+
+# Finding specific epithets that don't match att all
+epithetDat <- unique(datAll$Species)
+epithetDat[!sapply(epithetDat, FUN=function(x){any(grepl(x, sppReal))})]
+
+treeLists[treeLists$PlantID=="185-92*2",]
+listProb <- treeLists[grepl("Quercus", treeLists$Taxon) & treeLists$List==4,] # Replace 4 with whatever list the trouble maker is assigned to; then search by that observer's name & possible a date
+listProb[order(listProb$PlantID),]
+
+
+# Checking for trees whose genus + species don't match our list
+sppDat <- paste(datAll$Genus, datAll$Species)
+sppReal <- unique(treeLists$Taxon)
+
+
+# Need to find a way to remove leading spaces from specific epithets
+unique((datAll[!paste(datAll$Genus, datAll$Species) %in% c(unique(treeLists$Taxon)),c("Species")]))
+# treeLists[grep("humidicola", treeLists$Taxon),]
+# datAll[datAll$Species=="humidicola",]
+unique(paste(datAll$Genus[datAll$Species=="humidicola"], datAll$Species[datAll$Species=="humidicola"])) %in% treeLists$Taxon
+grep("microcarpa", treeLists$Taxon)
+treeLists[treeLists$PlantID=="15-2008*1",]
+
+
+unique(sppDat)
+unique(treeLists$Taxon)
+head(treeLists)
+summary(datAll[!paste(datAll$Genus, datAll$Species) %in% c(treeLists$Taxon),c("PlantID", "ObserverID", "Genus", "Species", "DateEntered")])
+length(unique((datAll[!paste(datAll$Genus, datAll$Species) %in% c(treeLists$Taxon),c("PlantID")])))
+length(unique((datAll[!paste(datAll$Genus, datAll$Species) %in% c(treeLists$Taxon),c("Species")])))
+length(unique((datAll[!paste(datAll$Genus, datAll$Species) %in% c(treeLists$Taxon),c("ObserverID")])))
+
+unique
+unique(paste(datAll$Genus, datAll$Species, sep=""))
+
+
+# PlantIDAll <- unique(datAll$PlantID)
+datAll[grepl("[/]", datAll$PlantID),]
+datAll[!grepl("[-]", datAll$PlantID) | !grepl("[*]", datAll$PlantID),]
+
+summary(datAll$ObserverID)
 
 # Deeper QAQC checks --> PlantIDs not in our list; PlantIDs don't match genus/species in entry; Observers not in our list
 
-# write.csv(TEST, "~/Google Drive/My Drive/LivingCollections_Phenology/LivingCollectionPhenology_ObservationData_LATEST.csv", row.names=F)
+
+# write.csv(datAll, "~/Google Drive/My Drive/LivingCollections_Phenology/LivingCollectionPhenology_ObservationData_LAdatAll.csv", row.names=F)
