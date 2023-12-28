2a9c11761ef068082510556a1318de3d6759a801
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2023-12-28 13:19:31 -0600

    Update PgAdmin_connect_and query.R
    added query for incorrect dates

--- scripts/PgAdmin_connect_and query.R
+++ scripts/PgAdmin_connect_and query.R
@@ -1,6 +1,7 @@
 ###Quick che ck  of uncleaned data from pgadmin 
 # Install and load the RPostgreSQL package
 library(RPostgreSQL)
+library(lubridate)
 
 # Set up database connection categories found under the connection tab of the properties of the Arboretum Phenology server in PgAdmin4. 
 db_host <- "164.92.83.213" #host name/address e  
@@ -28,6 +29,26 @@ if (inherits(con, "PostgreSQLConnection"))
 }
 
 ##query and manipulate the data here
+
+# List tables in the public schema
+tables <- dbGetQuery(con, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+cat("Tables in the public schema:\n", tables$table_name, "\n\n")
+
+# Query the "FormSubmission" table
+form_submission_data <- dbGetQuery(con, 'SELECT * FROM public."FormSubmission"')
+
+# Preview of FormSubmission data
+cat("Preview of FormSubmission data:\n")
+print(head(form_submission_data))
+#renaming because I don't want to copy and paste "form_submission_data" every time. 
+dat.all<- form_submission_data
+
+# Identify problematic years
+dat.all$DateObserved <- as.Date(dat.all$DateObserved)
+yrs.bad <- which(year(dat.all$DateObserved) != year(Sys.Date()))
+by.dat <- dat.all[yrs.bad, ]
+# Print the years that do not match
+print(by.dat)
 
 
 # Disconnect from the data base
