254b8b68417c00f58ae2671ca88c989d167b032f
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2023-12-28 14:08:38 -0600

    Update PgAdmin_connect_and query.R
    updated to find date entries which are outside of the system year, or data entries which may not conform to the as.date format.

--- scripts/PgAdmin_connect_and query.R
+++ scripts/PgAdmin_connect_and query.R
@@ -28,28 +28,42 @@ if (inherits(con, "PostgreSQLConnection"))
   cat("Failed to connect to PostgreSQL database")
 }
 
-##query and manipulate the data here
+#######query and manipulate the data here######
 
+##putting the pgadmin data into a data frame
 # List tables in the public schema
 tables <- dbGetQuery(con, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
-cat("Tables in the public schema:\n", tables$table_name, "\n\n")
 
-# Query the "FormSubmission" table
-form_submission_data <- dbGetQuery(con, 'SELECT * FROM public."FormSubmission"')
+# select the "FormSubmission" and put into the dat.all data fram, this is the table containing all the data entered from the app. 
+dat.all <- dbGetQuery(con, 'SELECT * FROM public."FormSubmission"')
 
-# Preview of FormSubmission data
-cat("Preview of FormSubmission data:\n")
-print(head(form_submission_data))
-#renaming because I don't want to copy and paste "form_submission_data" every time. 
-dat.all<- form_submission_data
+# Preview of columns in FormSubmission data frame dat.all
+print(head(dat.all))
 
-# Identify problematic years
+#### Identify problematic years###
+
+#setting the values in  DateObserved column to a date
 dat.all$DateObserved <- as.Date(dat.all$DateObserved)
-yrs.bad <- which(year(dat.all$DateObserved) != year(Sys.Date()))
-by.dat <- dat.all[yrs.bad, ]
+
+# Check for entries that do not have an entry for "DateObserved" or a funky or weird date, by funky or weird
+# I mean values that don't fit the as.Date format
+nodate <- which(is.na(dat.all$DateObserved))
+
+#if statement which prints rows with no or weird values for the Dateobserved column,
+if (length(nodate) > 0) {
+  cat("Rows with missing values in DateObserved:\n")
+  print(dat.all[nodate, ])
+} 
+##quick range check to what may be outside of the range 
+range(dat.all$DateObserved)
+
+#narrowing down those entries which may be outside of the year of phenology monitoring. 
+badyr <- which(year(dat.all$DateObserved) != year(Sys.Date()))
+by.dat <- dat.all[badyr, ]
 # Print the years that do not match
 print(by.dat)
 
+##Separating out entries into 
 
 # Disconnect from the data base
 dbDisconnect(con)
