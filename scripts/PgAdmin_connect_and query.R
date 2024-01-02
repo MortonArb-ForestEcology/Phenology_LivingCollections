fc34d547aee07cd14882357695b51a710875264d
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2024-01-02 15:27:19 -0600

    Update PgAdmin_connect_and query.R
    added some graphing scripts for phenophases.

--- scripts/PgAdmin_connect_and query.R
+++ scripts/PgAdmin_connect_and query.R
@@ -2,7 +2,7 @@
 # Install and load the RPostgreSQL package
 library(RPostgreSQL)
 library(lubridate)
-
+setwd("~/Volumes/GoogleDrive/My DriveLivingCollections_Phenology/Figures/2023_Figures")
 # Set up database connection categories found under the connection tab of the properties of the Arboretum Phenology server in PgAdmin4. 
 db_host <- "164.92.83.213" #host name/address e  
 db_port <- 5432 #port
@@ -58,12 +58,73 @@ if (length(nodate) > 0) {
 range(dat.all$DateObserved)
 
 #narrowing down those entries which may be outside of the year of phenology monitoring. 
-badyr <- which(year(dat.all$DateObserved) != year(Sys.Date()))
+## setting the year to 2023, but this should be set to "Sys.Date" in the future.
+badyr <- which(year(dat.all$DateObserved) != 2023)
 by.dat <- dat.all[badyr, ]
 # Print the years that do not match
 print(by.dat)
 
-##Separating out entries into 
 
+##### Graphing phenophase intensity 
+### Breaking leaf buds
+dat.all$BreakingLeafBudsIntensity <- factor(
+  dat.all$BreakingLeafBudsIntensity,
+  levels = c("0", "<3", "3-10", "11-100", "101-1,000", "1,001-10,000", ">10,000"),
+  ordered = TRUE
+)
+
+#Get unique genera
+unique_genera <- unique(dat.all$Genus)
+
+
+# Loop through each genus and create a separate plot
+for (genus in unique_genera) {
+  genus_data <- dat.all %>%
+    filter(Genus == genus, BreakingLeafBudsObserved == "y")
+  
+  # Plotting using ggplot2
+  p <- ggplot(genus_data, aes(x = DateObserved, y = Species, color = BreakingLeafBudsIntensity)) +
+    geom_point(alpha = 0.5) +
+    scale_color_manual(values = c("0" = "white", "<3" = "red", "3-10" = "orange", 
+                                  "11-100" = "yellow", "101-1,000" = "green", 
+                                  "1,001-10,000" = "blue", ">10,000" = "violet")) +
+    labs(title = paste("Breaking Leaf Buds_Intensity -", genus), x = "Date Observed", y = "Species") +
+    theme_minimal() +
+    theme(plot.background = element_rect(fill = "white"))
+  
+ 
+  # Save the plot as a separate file (you can adjust the file format and name)
+  ggsave(paste("Genus_", gsub(" ", "_", genus), "_Breaking_Leaf_Intensity_Plot.png", sep = ""), plot = p)
+}
+##Leaves
+dat.all$LeafIntensity <- factor(
+  dat.all$LeafIntensity,
+  levels = c("0 %", "<5 %", "5-24 %", "24-49 %", "50-74 %", "75-94 %", ">95 %"),
+  ordered = TRUE
+)
+head(dat.all)
+#Get unique genera
+unique_genera <- unique(dat.all$Genus)
+
+
+# Loop through each genus and create a separate plot
+for (genus in unique_genera) {
+  genus_data <- dat.all %>%
+    filter(Genus == genus, LeafObserved == "y")
+  
+  # Plotting using ggplot2
+  p <- ggplot(genus_data, aes(x = DateObserved, y = Species, color = LeafIntensity)) +
+    geom_point(alpha = 0.5) +
+    scale_color_manual(values = c("0%" = "white", "<5%" = "red", "5-24 %" = "orange", 
+                                  "24-49 %" = "yellow", "50-74 %" = "green", 
+                                  "75-94 %" = "blue", ">95 %" = "violet")) +
+    labs(title = paste("Leaf Intensity -", genus), x = "Date Observed", y = "Species") +
+    theme_minimal() +
+    theme(plot.background = element_rect(fill = "white"))
+  
+  
+  # Save the plot as a separate file (you can adjust the file format and name)
+  ggsave(paste("Genus_", gsub(" ", "_", genus), "_Leaf_Intensity_Plot.png", sep = ""), plot = p)
+}
 # Disconnect from the data base
 dbDisconnect(con)
