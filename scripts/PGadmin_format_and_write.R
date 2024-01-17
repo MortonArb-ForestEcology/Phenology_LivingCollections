8bf8194c857e6097158801ec4a165dbaccb0b7e8
BrendonReidy <48529854+BrendonReidy@users.noreply.github.com>
2024-01-16 18:49:32 -0600

    Update PGadmin_format_and_write.R
    completed column formatting and reorganization to match previous format. added in checks, added in code to write out csv according to previous format. added script to disconnect from the data base.

--- scripts/PGadmin_format_and_write.R
+++ scripts/PGadmin_format_and_write.R
@@ -3,6 +3,7 @@ library(RPostgreSQL)
 library(googlesheets4)
 library(readr)
 library(bit)
+library(dplyr)
 ####Pathing
 #BR: Make this work later 
 #pgadminconnect
@@ -38,9 +39,21 @@ dat.pg23 <- dbGetQuery(con, 'SELECT * FROM public."FormSubmission"')
 ###Script to write out PGAdmin phenology data to match existing format of old phenolgy data 
 ## Renaming and organizing columns 
 # Get the column names of the pgadmin data
-pgcolumn_names <- names(dat.pg23)
-# Print the column names
-print(pgcolumn_names)
+names(dat.pg23)
+# Renaming genus and species columns to create a column "species which has both the genus and species names
+# and a column named collection which contains only the genus name
+colnames(dat.pg23)[which(colnames(dat.pg23) == "Species")] <- "delete"
+colnames(dat.pg23)[which(colnames(dat.pg23) == "Genus")] <- "Collection"
+names(dat.pg23)
+# Combining these two columns into the new Species column
+dat.pg23$Species <- paste(dat.pg23$Collection, dat.pg23$delete)
+names(dat.pg23)
+# deleteing the extra column titles "delete" here
+dat.pg23 <- dat.pg23[, !(names(dat.pg23) == "delete")]
+names(dat.pg23)
+
+dat.pg23$Year <- format(as.Date(dat.pg23$DateObserved), "%Y")
+names(dat.pg23)
 
 
 #get the column names of existing phenology data
@@ -48,29 +61,85 @@ print(pgcolumn_names)
 gs4_auth()
 ac19 <- read_csv("~/Google Drive/My Drive/LivingCollections_Phenology/Data_Observations/LivingCollectionPhenology_ObservationData_Acer_2019_FINAL.csv")
 View(ac19)
+names(ac19)
 
-gscolumn_names <- names(ac19)
-# Print the column names
-print(gscolumn_names)
+#renaming the column names of pgadmin data to match those of existing pheno data
+colnames(dat.pg23)<- c("PlantNumber","Observer","Collection","Timestamp","Date.Observed","leaf.present.observed",
+                       "leaf.breaking.buds.observed","leaf.color.observed","leaf.increasing.observed","leaf.falling.observed",
+                       "flower.buds.observed","flower.open.observed","flower.pollen.observed","fruit.present.observed",
+                       "fruit.ripe.observed","fruit.drop.observed","leaf.present.intensity","leaf.breaking.buds.intensity",
+                       "leaf.color.intensity","flower.buds.intensity","flower.open.intensity","flower.pollen.intensity",
+                       "fruit.present.intensity","fruit.ripe.intensity","fruit.drop.intensity","leaf.increasing.intensity",
+                       "Notes", "Species", "Year")
+#Check
+names(dat.pg23)
+ # Specify the new order of columns
+new_order <- c("Timestamp", "Observer", "Date.Observed", "Species", "PlantNumber",
+               "Notes", "leaf.breaking.buds.observed", "leaf.breaking.buds.intensity",
+               "leaf.present.observed", "leaf.present.intensity", "leaf.increasing.observed",
+               "leaf.increasing.intensity", "leaf.color.observed", "leaf.color.intensity",
+               "leaf.falling.observed", "flower.buds.observed", "flower.buds.intensity",
+               "flower.open.observed", "flower.open.intensity", "flower.pollen.observed",
+               "flower.pollen.intensity", "fruit.present.observed", "fruit.present.intensity",
+               "fruit.ripe.observed", "fruit.ripe.intensity", "fruit.drop.observed",
+               "fruit.drop.intensity", "Collection", "Year")
 
-d
-
- # Specify the new order of column names
-new_order <- c("DateEntered
-               
-               
-             
-               
-               PlantID", "ObserverID", "Genus", "Species", "DateObserved",
-               "LeafObserved", "BreakingLeafBudsObserved", "LeafColorObserved",
-               "LeafIncreasingInSizeObserved", "LeafFallingObserved", "FlowerBudsObserved",
-               "FlowerOpenObserved", "FlowerPollenObserved", "FruitObserved",
-               "FruitRipeObserved", "FruitDropObserved", "LeafIntensity",
-               "BreakingLeafBudsIntensity", "LeafColorObservedIntensity",
-               "FlowerBudsObservedIntensity", "FlowerOpenObservedIntensity",
-               "FlowerPollenObservedIntensity", "FruitObservedIntensity",
-               "FruitRipeObservedIntensity", "FruitDropObservedIntensity",
-               "LeafIncreasingInSizeIntensity", "Comments")
-
-# Reorder the columns in the dataframe
+# Reorganize the columns
 dat.pg23 <- dat.pg23[, new_order]
+
+# Viewing the and checking
+dat.pg23
+View(dat.pg23)
+### checking with rbind
+#dat.fake <- rbind(ac19,dat.pg23)
+
+table(dat.pg23$leaf.present.observed)
+###If this works ensure the values for y and n are changed to Yes and No,  and "d is "Did not look for" 
+colupdate <- c("leaf.present.observed", "leaf.breaking.buds.observed", 
+                       "leaf.color.observed", "leaf.increasing.observed", 
+                       "leaf.falling.observed", "flower.buds.observed", 
+                       "flower.open.observed", "flower.pollen.observed", 
+                       "fruit.present.observed", "fruit.ripe.observed", 
+                       "fruit.drop.observed")
+
+# Update "y" to "Yes" and "n" to "No" in the specified columns
+dat.pg23 <- dat.pg23 %>%
+  mutate_at(
+    vars(colupdate),
+    funs(
+      case_when(
+        . == "y" ~ "Yes",
+        . == "n" ~ "No",
+        . == "d" ~ "Did not look for",
+        TRUE ~ .
+      )
+    )
+  )
+### Checking to see with a table of the unique values in the leaf present column
+table(dat.pg23$leaf.present.observed)
+
+## If this works write out CSVs by collection
+
+# Using the unique collection names in the Collection column
+unique_collections <- unique(dat.pg23$Collection)
+
+for (collection_name in unique_collections) {
+  # Create a subset for the current collection
+  subset_data <- dat.pg23[dat.pg23$Collection == collection_name, ]
+  
+  # Extract the year for naming purposes
+  year <- unique(subset_data$Year)
+  
+  # Generate a CSV file name based on the specified format
+  csv_file_name <- paste0("LivingCollectionPhenology_ObservationData_", gsub(" ", "_", collection_name), "_", year, "_FINAL.csv")
+  
+  # Write out the subset to a CSV file - Brendon improve this pathing 
+  write.csv(subset_data, csv_file_name, row.names = FALSE)
+  
+  cat("CSV file", csv_file_name, "created for collection", collection_name, "and year", year, "\n")
+}
+
+
+##
+# Disconnect from the pgadming data base
+dbDisconnect(con)
