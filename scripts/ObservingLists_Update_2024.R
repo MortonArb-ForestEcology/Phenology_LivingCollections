b01ca8fd705337481d6339a5512ed1d48839dfbe
Christy Rollinson <crollinson@mortonarb.org>
2024-02-23 16:31:59 -0600

    Placeholder script for updating lists

---
+++ scripts/ObservingLists_Update_2024.R
@@ -0,0 +1,11 @@
+# Update the most recent round of observing lists to make sure removed trees are no longer listed
+
+# 1. Read in the existing lists --> ALWAYS START WITH THIS
+#
+
+# 2. Check for removed trees
+#    2.a. Pull our existing removed trees list
+#    2.b. Search last year's data for trees that say they're gone; 
+#    2.c. add them to the removed list if there are never any obs on them
+
+# 3. Update the existing lists to remove trees that we know are now gone
